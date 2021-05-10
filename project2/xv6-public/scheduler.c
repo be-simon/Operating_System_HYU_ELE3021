542e65d9d2eec7080dcb9d84109f59d28ec2dc35
정재용 <wodydthk7@hanyang.ac.kr>
2021-05-10 15:36:43 +0900

    project2 init

---
+++ project2/xv6-public/scheduler.c
@@ -0,0 +1,150 @@
+#include "types.h"
+#include "defs.h"
+#include "param.h"
+#include "mmu.h"
+#include "proc.h"
+
+struct scheduler mlfqsched = {{0, }, 0, TOTALTICKETS, 1, 0};
+struct scheduler stridesched = {{0, }, 0, 0, 0, 0};
+
+int
+set_cpu_share(int share){
+	int tickets = TOTALTICKETS * share / 100;
+	if (stridesched.tickets + tickets > TOTALTICKETS * MAXSHARE / 100)
+		return -1;
+
+	mlfqsched.tickets -= tickets;
+	stridesched.tickets += tickets;
+
+	if (stridesched.stride == 0)
+		stridesched.pass = mlfqsched.pass;
+
+	mlfqsched.stride = TOTALTICKETS / mlfqsched.tickets;
+	stridesched.stride = TOTALTICKETS / stridesched.tickets;
+
+	myproc()->tickets = tickets;
+	myproc()->stride = stridesched.tickets / tickets;
+	if (stridesched.count > 0)
+		myproc()->pass = stridesched.heap[1]->pass;
+	else
+		myproc()->pass = 0;
+
+	stride_enqueue(myproc());
+
+	return 0;
+}
+
+int
+mlfq_enqueue(struct proc *p){
+	// queue is full
+	if (mlfqsched.count >= NPROC){
+		return -1;
+	}
+
+	int parent;
+	int child;
+	
+	child = ++mlfqsched.count;
+	parent = child / 2;
+	
+	while (child > 1) {
+		if (p->qlev < mlfqsched.heap[parent]->qlev) {
+			mlfqsched.heap[child] = mlfqsched.heap[parent];
+			child = parent;
+			parent = child / 2;
+		} else
+			break;
+	}
+	mlfqsched.heap[child] = p;
+
+	return 0;
+}
+
+struct proc*
+mlfq_dequeue(void) {
+	// heap is empty
+	if (mlfqsched.count == 0){
+		return 0;
+	}
+
+	struct proc *p = mlfqsched.heap[1];
+	struct proc *end = mlfqsched.heap[mlfqsched.count--];
+	int parent = 1;
+	int child = 2;
+
+	while (child <= mlfqsched.count) {
+		if (child < mlfqsched.count) {
+			child = (mlfqsched.heap[child]->qlev < mlfqsched.heap[child + 1]->qlev) ? child : child + 1;
+		}
+
+		if (end->qlev >= mlfqsched.heap[child]->qlev) {
+			mlfqsched.heap[parent] = mlfqsched.heap[child];
+			parent = child;
+			child = parent * 2;
+		} else 
+			break;
+	}
+
+	if (mlfqsched.count > 0)
+		mlfqsched.heap[parent] = end;
+
+	return p;
+}
+
+int
+stride_enqueue(struct proc *p){
+	// queue is full
+	if (stridesched.count >= NPROC){
+		return -1;
+	}
+
+	int parent;
+	int child;
+	
+	child = ++stridesched.count;
+	parent = child / 2;
+	
+	while (child > 1) {
+		if (p->pass < stridesched.heap[parent]->pass) {
+			stridesched.heap[child] = stridesched.heap[parent];
+			child = parent;
+			parent = child / 2;
+		} else
+			break;
+	}
+	stridesched.heap[child] = p;
+
+	return 0;
+}
+
+struct proc*
+stride_dequeue(void) {
+	// heap is empty
+	if (stridesched.count == 0){
+		return 0;
+	}
+
+	struct proc *p = stridesched.heap[1];
+	struct proc *end = stridesched.heap[stridesched.count--];
+	int parent = 1;
+	int child = 2;
+
+	while (child <= stridesched.count) {
+		if (child < stridesched.count) {
+			child = (stridesched.heap[child]->pass < stridesched.heap[child + 1]->pass) ? child : child + 1;
+		}
+
+		if (end->pass >= stridesched.heap[child]->pass) {
+			stridesched.heap[parent] = stridesched.heap[child];
+			parent = child;
+			child = parent * 2;
+		} else 
+			break;
+	}
+
+	if (stridesched.count > 0)
+		stridesched.heap[parent] = end;
+
+	return p;
+}
+
